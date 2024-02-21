7c0018b20d31d945114abd1b9b075c33ceaa5075
Neel Patel <np.patelneel@gmail.com>
2024-02-20 21:32:56 -0800

    added bottom bar navigation with dummy screens

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,17 +1,26 @@
 import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AuthContext } from "../provider/AuthProvider";
 import { auth } from "../../firebase.config";
 
-// Main
+// Main Screens
 import Home from "../screens/Home";
 import SecondScreen from "../screens/SecondScreen";
 
-// Auth screens
+// Auth Screens
 import Login from "../screens/auth/Login";
 import Register from "../screens/auth/Register";
 import ForgetPassword from "../screens/auth/ForgetPassword";
+
+// Additional Tab Screens
+import SearchScreen from "../screens/SearchScreen";
+import CartScreen from "../screens/CartScreen";
+import OrdersScreen from "../screens/OrdersScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import SellScreen from "../screens/SellScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 
 import Loading from "../screens/utils/Loading";
 
@@ -46,6 +55,26 @@ const Main = () => {
   );
 };
 
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen name="Orders" component={OrdersScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Sell" component={SellScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+};
+
 export default () => {
   const authContext = useContext(AuthContext);
   const user = authContext.user;
@@ -54,7 +83,7 @@ export default () => {
     <NavigationContainer>
       {user === null && <Loading />}
       {user === false && <Auth />}
-      {user === true && <Main />}
+      {user === true && <TabNavigator />}
     </NavigationContainer>
   );
 };
